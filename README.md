a82a366ace36a4fa9a1cb6f1204d338db2bd0565
konpapanik <hello@devpap.co.uk>
2024-05-09 11:00:17 +0100

    initial make

---
+++ README.md
@@ -0,0 +1,12 @@
+# WooCommerce product Reviews Schema
+
+Adds schema markup for WooCommerce product reviews.
+
+
+## Contents
+
+* `.gitignore`. Used to exclude certain files from the repository.
+* `README.md`. The file that you’re currently reading.
+* A `woo-products-reviews-schema` directory that contains the source code - a fully executable WordPress plugin.
+
+## Credits
--- woo-products-reviews-schema.php
+++ woo-products-reviews-schema.php
@@ -1,38 +1,69 @@
 <?php
 /*
-Plugin Name: WooCommerce product Reviews Schema
+Plugin Name: Custom Woo product Reviews Schema
 Description: Adds schema markup for WooCommerce product reviews.
 Version: 1.0
-Author: Your Name
-*/
+Plugin URI:  https://cybergogo.co.uk/
+Author: Konstantinos Pap Cybergogo
+Author URI:  https://cybergogo.co.uk/
+Author Email:
 
-
-
-<?php
-/**
- * Plugin Name: Buy Once or Subscribe for WooCommerce Subscriptions
- * Plugin URI:  https://woocommerce.com/products/buy-once-or-subscribe-for-woocommerce-subscriptions/
- * Description: This plugin allows shop owners to easily add subscriptions to existing WooCommerce Simple Products.
- * Version:     4.0.0
- * Author:      eCommerce Tools
- * Author URI:  https://ecommercetools.io
- *
- * Text Domain: bos4w
- * Domain Path: /languages/
- *
- * Requires PHP: 7.2
- *
- * Requires at least: 5.7.0
- * Tested up to: 6.2.2
- *
- * Woo: 18734000055043:c1a7780fb14b3ad8f3d13241e25f4dac
- * WC requires at least: 6.8.0
- * WC tested up to: 7.9.0
- *
- * Copyright: © 2021 eCommerce Tools
+ * Copyright: © 2024 cybergogo
  * License: GNU General Public License v3.0
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
- *
- * @package Buy Once or Subscribe for WooCommerce Subscriptions
- * @since 1.0.0
- */
+*/
+
+// Add WooCommerce product reviews schema
+function add_woocommerce_product_reviews_schema() {
+    // Check if WooCommerce is active
+    if ( ! class_exists( 'WooCommerce' ) ) {
+        return;
+    }
+
+    // Get the current product
+    global $product;
+
+    // Check if we are on a single product page and if the product has reviews
+    if ( is_product() && $product && $product->get_review_count() > 0 ) {
+        $schema = array(
+            '@context'        => 'http://schema.org',
+            '@type'           => 'Product',
+            'aggregateRating' => array(
+                '@type'       => 'AggregateRating',
+                'ratingValue' => $product->get_average_rating(),
+                'reviewCount' => $product->get_review_count(),
+            ),
+            'review'          => array(),
+        );
+
+        // Get product reviews
+        $args = array(
+            'post_id' => $product->get_id(),
+            'status'  => 'approve',
+            'type'    => 'review',
+        );
+        $reviews = get_comments( $args );
+
+        if ( $reviews ) {
+            foreach ( $reviews as $review ) {
+                $schema['review'][] = array(
+                    '@type'         => 'Review',
+                    'author'        => array(
+                        '@type' => 'Person',
+                        'name'  => get_comment_author( $review->comment_ID ),
+                    ),
+                    'datePublished' => get_comment_date( 'c', $review->comment_ID ),
+                    'description'   => wp_strip_all_tags( $review->comment_content ),
+                    'reviewRating'  => array(
+                        '@type'       => 'Rating',
+                        'ratingValue' => get_comment_meta( $review->comment_ID, 'rating', true ),
+                    ),
+                );
+            }
+        }
+
+        // Output the schema markup
+        echo '<script type="application/ld+json">' . wp_json_encode( $schema ) . '</script>';
+    }
+}
+add_action( 'wp_footer', 'add_woocommerce_product_reviews_schema' );
