99dd11043ed8a9916d2d975e591ea5c09762652b
devpap <devpap@devpaps-Mac-mini.local>
2024-05-09 10:23:17 +0100

    initial setup

---
+++ woo-products-reviews-schema.php
@@ -0,0 +1,38 @@
+<?php
+/*
+Plugin Name: WooCommerce product Reviews Schema
+Description: Adds schema markup for WooCommerce product reviews.
+Version: 1.0
+Author: Your Name
+*/
+
+
+
+<?php
+/**
+ * Plugin Name: Buy Once or Subscribe for WooCommerce Subscriptions
+ * Plugin URI:  https://woocommerce.com/products/buy-once-or-subscribe-for-woocommerce-subscriptions/
+ * Description: This plugin allows shop owners to easily add subscriptions to existing WooCommerce Simple Products.
+ * Version:     4.0.0
+ * Author:      eCommerce Tools
+ * Author URI:  https://ecommercetools.io
+ *
+ * Text Domain: bos4w
+ * Domain Path: /languages/
+ *
+ * Requires PHP: 7.2
+ *
+ * Requires at least: 5.7.0
+ * Tested up to: 6.2.2
+ *
+ * Woo: 18734000055043:c1a7780fb14b3ad8f3d13241e25f4dac
+ * WC requires at least: 6.8.0
+ * WC tested up to: 7.9.0
+ *
+ * Copyright: © 2021 eCommerce Tools
+ * License: GNU General Public License v3.0
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * @package Buy Once or Subscribe for WooCommerce Subscriptions
+ * @since 1.0.0
+ */
